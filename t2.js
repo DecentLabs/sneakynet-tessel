8500a115f75b78f97afd92fa9d9ec3dc40efede8
Fejes Aniko <oranjje@gmail.com>
2017-07-16 17:11:57 +0200

    blink blink

--- t2.js
+++ t2.js
@@ -1,6 +1,17 @@
 const ap = require('./ap')
 const server = require('./server')
+const tessel = require('tessel')
+
+// Turn one of the LEDs on to start.
+tessel.led[2].on()
 
 // launch tessel access point
 ap.create()
+
+// Blink!
+setInterval(() => {
+  tessel.led[2].toggle()
+}, 100)
+
+// launch server
 server.listen()
